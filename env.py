189cc6a75a4bb567c2d4533ed1f4becbdd44d293
GaryCooper-pm <gary@pixelmixel.co.uk>
2022-09-04 15:23:26 +0000

    Update .gitignore to comment out env.py to enable multi day coding

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://vflemxgmlgrtbl:2d08afe471a9ef6bf2cd5bbbee542d8e5b4de53fc4d8306c36bf39d53dfd73b9@ec2-52-212-228-71.eu-west-1.compute.amazonaws.com:5432/detg2s2gt0vsgu"
+os.environ["SECRET_KEY"] = "L0aGHtanL0aDED2022!!"
