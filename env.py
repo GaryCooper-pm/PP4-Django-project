9bde458f6a16e6a73676dc5a628038acaf4659e4
GaryCooper-pm <gary@pixelmixel.co.uk>
2022-09-05 09:09:31 +0000

    Updatee env.py with new DATABASE_URL due to Heroku maintenance

--- env.py
+++ env.py
@@ -1,4 +1,4 @@
 import os
 
-os.environ["DATABASE_URL"] = "postgres://vflemxgmlgrtbl:2d08afe471a9ef6bf2cd5bbbee542d8e5b4de53fc4d8306c36bf39d53dfd73b9@ec2-52-212-228-71.eu-west-1.compute.amazonaws.com:5432/detg2s2gt0vsgu"
+os.environ["DATABASE_URL"] = "postgres://sjatiqucfoltvp:56308a8587c748788c9abb9468eb81ae1ae667ad443ccc5d7c90b3e3c7ad2285@ec2-52-211-182-159.eu-west-1.compute.amazonaws.com:5432/d8mllgriin1rb6"
 os.environ["SECRET_KEY"] = "L0aGHtanL0aDED2022!!"
