053041d38a231a10f19ca6c2942deb3550bf98a7
GaryCooper-pm <gary@pixelmixel.co.uk>
2022-09-07 15:52:19 +0000

    Update settings.py to comment out X_Frame and clickjacking to try and get AmIResponsive to work

--- trailsandales/settings.py
+++ trailsandales/settings.py
@@ -79,7 +79,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'trailsandales.urls'
